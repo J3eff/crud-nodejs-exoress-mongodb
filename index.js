314f5efb4097890073f63c0a5caed3fd9193e99a
J3eff <jeffersonvideo125@gmail.com>
2022-08-04 00:29:04 -0300

    Implementado endpoint de post e get

---
+++ index.js
@@ -0,0 +1,36 @@
+//config inicial
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+
+
+// forma de ler JSON / middlewares
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+// rotas da API
+const personRoutes = require('./routes/personRoutes')
+
+app.use('/person', personRoutes)
+
+// rota inicial / endpoint
+app.get("/", (req, res) => {
+  res.json({ message: "Oi Express!" });
+});
+
+//entregar uma porta
+mongoose
+  .connect(
+    "mongodb+srv://xTapee:Nhd8hinyeW3m0WGY@apicluster.qhlqu.mongodb.net/bancodaapi?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connectamos ao MongoDB");
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
---
+++ routes/personRoutes.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const Person = require("../models/Person");
+
+// Create
+router.post("/", async (req, res) => {
+  // { name: "Jefferson", salary: 5000, approved: false }
+  const { name, salary, approved } = req.body;
+
+  if (!name) {
+    res.status(422).json({ error: "O nome é obrigatório!" });
+  }
+
+  const person = {
+    name,
+    salary,
+    approved,
+  };
+
+  // create
+  try {
+    await Person.create(person);
+
+    res
+      .status(201)
+      .json({ message: "Pessoa inserida no sistema com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+// Read
+router.get("/", async (req, res) => {
+  try {
+    const people = await Person.find();
+
+    res.status(200).json(people);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  //extrair o dado da requisição, pela url = req.params
+  const id = req.params.id;
+
+  try {
+    const person = await Person.findOne({ _id: id });
+
+    res.status(200).json(person);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+module.exports = router;
